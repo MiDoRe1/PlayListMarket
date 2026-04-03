87b90a112617959547a9e27ec0e3b68f5f25911d
Practicum <practicum@ya.ru>
2026-04-04 00:44:10 +0300

    8 спринт

---
+++ app/src/main/java/com/example/playlistmarket/SettingsActivity.kt
@@ -0,0 +1,20 @@
+package com.example.playlistmarket
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val imageGoBack = findViewById<ImageView>(R.id.image_back)
+        imageGoBack.setOnClickListener {
+            finish()
+        }
+    }
+}

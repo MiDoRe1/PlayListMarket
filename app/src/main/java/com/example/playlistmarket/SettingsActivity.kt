809d9561aee710a986306df32fd17d4ee085db22
Practicum <practicum@ya.ru>
2026-04-05 20:09:44 +0300

    9 спринт. 2 ДЗ. Внес правки (перенес строки и ссылку в отдельный файл)

--- app/src/main/java/com/example/playlistmarket/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmarket/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.playlistmarket
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textview.MaterialTextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,9 +25,39 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         val settingsToolBar = findViewById<MaterialToolbar>(R.id.header_toolbar)
-
         settingsToolBar.setNavigationOnClickListener {
             finish()
+        }
+
+        val buttonShareApp = findViewById<MaterialTextView>(R.id.buttonShareApp)
+        buttonShareApp.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(
+                Intent.EXTRA_TEXT,
+                getString(R.string.yandex_andriod_developer_course)
+            )
+            startActivity(shareIntent)
+        }
+
+        val buttonWriteToSupport = findViewById<MaterialTextView>(R.id.buttonWriteToSupport)
+        buttonWriteToSupport.setOnClickListener {
+            val mailIntent = Intent(Intent.ACTION_SENDTO)
+            mailIntent.data = Uri.parse("mailto:")
+            mailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("myWorkMail@mail.com"))
+            mailIntent.putExtra(
+                Intent.EXTRA_SUBJECT,
+                getString(R.string.subject_for_email_to_support))
+            mailIntent.putExtra(Intent.EXTRA_TEXT,
+                R.string.text_for_email_to_support)
+            startActivity(mailIntent)
+        }
+
+        val buttonUserAgreement = findViewById<MaterialTextView>(R.id.buttonUserAgreement)
+        buttonUserAgreement.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW)
+            browserIntent.data = Uri.parse("https://yandex.ru/legal/practicum_offer/ru/")
+            startActivity(browserIntent)
         }
     }
 }

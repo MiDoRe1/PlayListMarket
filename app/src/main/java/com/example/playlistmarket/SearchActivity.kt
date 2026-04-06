69ea953ffda34e37d374955b238c4f6194a10a78
Practicum <practicum@ya.ru>
2026-04-06 21:00:39 +0300

    9 спринт. 3 ДЗ. Добавил сохрание текста из TextEdit в InstanceState.

--- app/src/main/java/com/example/playlistmarket/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmarket/SearchActivity.kt
@@ -1,11 +1,19 @@
 package com.example.playlistmarket
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +25,65 @@ class SearchActivity : AppCompatActivity() {
             v.updatePadding(top = statusBar.top)
             insets
         }
+
+        val searchTextInput = findViewById<TextInputLayout>(R.id.searchTextInput)
+        val searchEditText = findViewById<TextInputEditText>(R.id.searchEditText)
+
+        searchTextInput.setEndIconOnClickListener {
+            searchEditText.text?.clear()
+            searchTextInput.clearFocus()
+        }
+
+        if (savedInstanceState != null) {
+            val searchTextFromBundle = savedInstanceState.getString(
+                INPUT_TEXT_FOR_SEARCH,
+                DEFAULT_TEXT_FOR_SEARCH)
+            searchEditText.setText(searchTextFromBundle)
+        }
+
+        searchEditText.addTextChangedListener(
+            object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {}
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+
+            }
+
+        })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val editText = findViewById<TextInputEditText>(R.id.searchEditText)
+        outState.putString(INPUT_TEXT_FOR_SEARCH ,editText.getText().toString())
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle?,
+        persistentState: PersistableBundle?
+    ) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState)
+        val searchEditText = findViewById<TextInputEditText>(R.id.searchEditText)
+        if (savedInstanceState != null) {
+            val searchTextFromBundle = savedInstanceState.getString(
+                INPUT_TEXT_FOR_SEARCH,
+                DEFAULT_TEXT_FOR_SEARCH)
+            searchEditText.setText(searchTextFromBundle)
+        }
+    }
+    companion object {
+        val INPUT_TEXT_FOR_SEARCH : String = "INPUT_TEXT_FOR_SEARCH"
+        val DEFAULT_TEXT_FOR_SEARCH: String = ""
     }
 }

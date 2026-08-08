85b557340e37b0f2bd7abd6ec3d05cf7062036b6
Practicum <practicum@ya.ru>
2026-08-08 14:55:24 +0300

    15 спринт. Исправил API

--- app/src/main/java/com/example/playlistmarket/data/network/ITunesSearchAPI/ITunesSearchAPIInterface.kt
+++ app/src/main/java/com/example/playlistmarket/data/network/ITunesSearchAPI/ITunesSearchAPIInterface.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ITunesSearchAPIInterface {
-    @GET("search")
+    @GET("search?entity=song")
     fun getTracks(@Query("term") text:String): Call<TrackInfoResponse>
 
 }
